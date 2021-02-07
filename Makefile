6998b99bf7b9801250c0ab784c91178f7488c767
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2021-02-07 17:44:22 +0530

    basic lexer

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-all: /bin/lexer
-bin/lexer: lex.yy.c 
-	mkdir bin
+all: bin/lexer
+bin/lexer: y.tab.h lex.yy.c 
+	mkdir -p bin
 	gcc -Isrc/ lex.yy.c src/lexforc.c -o bin/lexer
 	rm lex.yy.c
+	rm y.tab.c
+	rm y.tab.h
+y.tab.h: src/y.y
+	yacc -d src/y.y
 lex.yy.c: src/lexforc.l
 	lex src/lexforc.l
 clean:
---
+++ src/lexforc.c
@@ -0,0 +1,44 @@
+#include "lexforc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int yylex();
+extern int yylineno;
+extern int column;
+extern char* yytext;
+extern FILE *yyin;
+
+char *getToken(int tokenid) {
+  if (tokenid > 30 && tokenid < 127) {
+    char s[2];
+    sprintf(s, "%c", (char)tokenid);
+    return strdup(s);
+  } else {
+    return tokens[tokenid];
+  }
+}
+
+int main(int argc, char* argv[]) {
+  int tokenid;
+  initTokens();
+  FILE *f;
+  if (argc == 2) {
+    f = fopen(argv[1], "r");
+    if (f) {
+      yyin = f;
+    } else {
+      fprintf(stderr, "Input File Does Exist.\n");
+      return 0;
+    }
+  } else {
+    fprintf(stderr, "This Program requires 1 argument.\n");
+    return 0;
+  }
+  printf("      Token           Lexeme     Line#  Column#\n");
+  while (tokenid = yylex()) {
+    //printf("> TOKEN_ID: %d, YYLINE_NO: %d, COLUMN: %d, YYTEXT: %s\n", tokenid, yylineno, column, yytext);
+    printf("%12s %15s %7d %7d\n", getToken(tokenid), yytext, yylineno, column);
+  }
+  return 0;
+}
